275fcfb759ac4bae754f493a9ec0e99ecdfbd925
Sandy Walsh <sandy.walsh@rackspace.com>
2013-03-29 13:29:46 -0300

    Proper bootstrap and better forms

--- thatwas_server.py
+++ thatwas_server.py
@@ -1,5 +1,6 @@
 import datetime
 
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask import flash
 from flask import redirect
@@ -15,13 +16,35 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key = "secret"
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/thatwas.db'
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(15), index=True)
+    author = db.Column(db.String(35))
+    start = db.Column(db.DateTime(), index=True)
+    end = db.Column(db.DateTime(), index=True)
+    description = db.Column(db.String(80))
+
+    def __init__(self, tag, author, start, end, description):
+        self.tag = tag
+        self.author = author
+        self.start = start
+        self.end = end
+        self.description = description
+
+    def __repr__(self):
+        return "<Event '%r'>" % self.tag
+
 
 class EventForm(Form):
     tag = TextField('Tag', [validators.Length(min=1, max=15)])
     author = TextField('Email Address', [validators.Length(min=6, max=35)])
-    start_datetime = DateTimeField('Start', [validators.Required()],
+    start_datetime = DateTimeField('Start Date/Time', [validators.Required()],
                                             default=datetime.datetime.utcnow())
-    end_datetime = DateTimeField('End',
+    end_datetime = DateTimeField('End Date/Time',
                                 [validators.Required()],
                                 default=datetime.datetime.utcnow() +
                                         datetime.timedelta(days=1))
@@ -33,7 +56,6 @@ def index():
     form = EventForm(request.form)
     if request.method == 'POST' and form.validate():
         flash("The '%s' event has been recorded. Thanks!" % form.tag.data)
-        print "FLASH"
         return redirect(url_for('index'))
     return render_template('index.html', form=form)
 
